344c9d37ed9a8bc0553dab63330cb03807a22be4
Bradley Davis <bad884@me.com>
2018-11-13 17:54:01 -0600

    Initial changes

--- src/implementation/colorhierarchy.js
+++ src/implementation/colorhierarchy.js
@@ -2,7 +2,8 @@ export default function(hierarchyRoot, colormap) {
     let colorIndex = 0;
     hierarchyRoot.children.forEach(function(child) {
         child.descendants().forEach(function(desc){
-            desc.color = colormap[colorIndex % colormap.length];
+            // desc.color = colormap[colorIndex % colormap.length];
+            desc.color = colormap[desc.name % colormap.length];
         });
         colorIndex++;
     });
