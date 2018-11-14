2358006fce4dc00427d42306442f6cab8f237a6c
Bradley Davis <bad884@me.com>
2018-11-13 23:03:16 -0600

    Add more nodes

--- src/implementation/colorhierarchy.js
+++ src/implementation/colorhierarchy.js
@@ -3,8 +3,14 @@ export default function(hierarchyRoot, colormap) {
     hierarchyRoot.children.forEach(function(child) {
         child.descendants().forEach(function(desc){
             // desc.color = colormap[colorIndex % colormap.length];
-            desc.color = colormap[desc.name % colormap.length];
+            desc.color = colormap[hashCode(desc.data.name) % colormap.length];
+            console.log(desc.data.name + ": " + hashCode(desc.data.name));
         });
         colorIndex++;
     });
+}
+
+function hashCode(str) {
+  return str.split('').reduce((prevHash, currVal) =>
+    (((prevHash << 5) - prevHash) + currVal.charCodeAt(0))|0, 0);
 }
